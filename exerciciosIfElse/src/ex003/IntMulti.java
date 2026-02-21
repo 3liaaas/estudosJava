0b2506e16f0fbd884bb4841a86e0337c83d491b3
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-02-21 19:26:37 -0300

    exercícios de impar e par

---
+++ exerciciosIfElse/src/ex003/IntMulti.java
@@ -0,0 +1,18 @@
+package ex003;
+import java.util.Scanner;
+
+public class IntMulti {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if(A % B == 0 || B % A == 0){
+            System.out.println("São Múltiplos");
+        } else {
+            System.out.println("Não são múltiplos");
+        }
+        sc.close();
+    }
+}
