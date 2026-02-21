fb0917402a179c0e3104c84ef6ac1415f1d838e6
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-02-21 19:16:10 -0300

    exercícios de impar e par

---
+++ exerciciosIfElse/src/ex002/Main.java
@@ -0,0 +1,16 @@
+package ex002;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+            if (x % 2 == 0){
+            System.out.println("È pá");
+
+        }   else {
+            System.out.println("É impá");
+        }
+        sc.close();
+    }
+}
