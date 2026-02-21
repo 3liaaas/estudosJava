b7066c151ee0b20d6dab43dfe49bb6f5631c984f
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-02-21 20:24:22 -0300

    fast food exercicio

---
+++ exerciciosIfElse/src/ex005/FastFood.java
@@ -0,0 +1,15 @@
+package ex005;
+import java.util.Scanner;
+
+public class FastFood {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int codigo = sc.nextInt();
+        int quantidade = sc.nextInt();
+
+        double total;
+        if(codigo == 1){
+            total - quantidade * 4.0
+    }
+}
