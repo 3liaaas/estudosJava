3028f6e9c68f7ee94cd6242c11003200f1b344d5
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-03-07 23:18:33 -0300

    Update FastFood.java

--- exerciciosIfElse/src/ex005/FastFood.java
+++ exerciciosIfElse/src/ex005/FastFood.java
@@ -2,14 +2,20 @@ package ex005;
 import java.util.Scanner;
 
 public class FastFood {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int codigo = sc.nextInt();
         int quantidade = sc.nextInt();
 
         double total;
-        if(codigo == 1){
-            total - quantidade * 4.0
+        if (codigo == 1) {
+            total = quantidade * 4.0;
+        } else if(codigo == 2){
+            total = quantidade * 4.50;
+        } else if (codigo == 3) {
+            total = quantidade * 5.00
+        }
+        }
     }
 }
