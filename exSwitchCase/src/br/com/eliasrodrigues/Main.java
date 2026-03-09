97be032b224de5dac0aa6e5b26d12b147ab892e3
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-03-09 11:04:39 -0300

    teste com switch case

---
+++ exSwitchCase/src/br/com/eliasrodrigues/Main.java
@@ -0,0 +1,12 @@
+package br.com.eliasrodrigues;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        String dia;
+
+        switch (x){}
+    }
+}
---
+++ exSwitchCase/src/br/com/eliasrodrigues/uri002.java
@@ -0,0 +1,19 @@
+package br.com.eliasrodrigues;
+import java.util.Scanner;
+public class uri002 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int X = A + B;
+
+        System.out.printf("X = %d\n", X);
+        sc.close();
+
+
+    }
+}
+
+
+
