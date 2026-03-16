f198e22d09b587bf32648a32ec258570f777dbc9
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-03-16 11:00:58 -0300

    ex002 feito

---
+++ estruturaFor/src/br/com/exercicios/eliasrodrigues/Ex002.java
@@ -0,0 +1,9 @@
+package br.com.exercicios.eliasrodrigues;
+import java.util.Scanner;
+
+public class Ex002 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+    }
+}
