7d895f69692a5ce91bfa166a847a3ea5556988c9
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-03-17 11:48:24 -0300

    exercício 6 feito

--- estruturaFor/src/br/com/exercicios/eliasrodrigues/Ex005.java
+++ estruturaFor/src/br/com/exercicios/eliasrodrigues/Ex005.java
@@ -1,5 +1,21 @@
 package br.com.exercicios.eliasrodrigues;
-import java
+import java.util.Scanner;
+
 
 public class Ex005 {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+
+        int fatorial = 1;
+
+        for (int i=1;i<=n;i++){
+            fatorial = fatorial * i;
+        }
+        sc.close();
+        System.out.println(fatorial);
+    }
 }
+
