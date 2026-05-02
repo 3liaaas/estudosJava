6147397c97c492963acb726fa35ac3bd7e674a3d
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-05-02 16:21:10 -0300

    formatando e manipulando Strings

---
+++ exerciciosEtestes/src/FuncoesString.java
@@ -0,0 +1,16 @@
+public class FuncoesString {
+    public static void main(String[] args) {
+        String original = "abcde FGHIJ ABC abc DEFG";
+
+        String s01 = original.toLowerCase();
+        String s02  = original.toUpperCase();
+        String s03  = original.trim();
+        String s04  = original.substring(2);
+
+        System.out.println(s01);
+        System.out.println(s02);
+        System.out.println(s03);
+        System.out.println(s04);
+
+    }
+}
