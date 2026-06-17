a1e9f78131cf12b2474637e7e814ec1301b2ce55
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-06-17 07:43:13 -0300

    Praticando POO

--- EstudoPooUdemy/src/Program/Main01.java
+++ EstudoPooUdemy/src/Program/Main01.java
@@ -1,28 +1,32 @@
-package entities;
+package Program;
 import java.util.Scanner;
+
+import entities.Triangle;
 
 public class Main01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
 
         System.out.println
             ("Enter the measures of triangule X: ");
-                xA = sc.nextDouble();
-                xB = sc.nextDouble();
-                xC = sc.nextDouble();
+                x.a = sc.nextDouble();
+                x.b = sc.nextDouble();
+                x.c = sc.nextDouble();
 
         System.out.println
             ("Enter the measures of triangule y: ");
-                yA = sc.nextDouble();
-                yB = sc.nextDouble();
-                yC = sc.nextDouble();
+                y.a = sc.nextDouble();
+                y.b = sc.nextDouble();
+                y.c = sc.nextDouble();
 
-                double p = (xA + xB + xC) / 2.0;
-                double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+                double p = (x.a + x.b + x.c) / 2.0;
+                double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 
-                p = (yA + yB + yC) / 2.0;
-                double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+                p = (y.a + y.b + y.c) / 2.0;
+                double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 
                 System.out.printf("triângulo X area: %.4f%n", areaX);
                 System.out.printf("triângulo y area: %.4f%n", areaY);
@@ -49,5 +53,10 @@ public class Main01 {
     * Para melhorar isso, vamos usar uma CLASSE para representar um triângulo.
     * */
 
+    /*
+    * Instanciação
+    * Váriaveis Clássicas são estáticas, ficam na área da memória "Stack"
+    * Váriaveis Dinâmicas ficam na área Heap da memória
+    * */
 
 }
--- EstudoPooUdemy/src/entities/Triangle.java
+++ EstudoPooUdemy/src/entities/Triangle.java
@@ -1,7 +1,14 @@
-package application;
+package entities;
 
 public class Triangle {
     public double a;
     public double b;
     public double c;
 }
+
+
+/*
+* Triangle é um tipo, que também é um objeto, X e Y são váriaveis
+* que apontam para um objeto, e esse objeto é composto por atributos
+* (citados acima)
+* */
