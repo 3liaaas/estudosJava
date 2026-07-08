f4e4ad4b0e9ee259b2381bd25acad11952c05c6b
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-07-08 11:10:02 -0300

    Praticando instanciação e métodos

---
+++ EstudoPooUdemy/src/Program/AppEmployee.java
@@ -0,0 +1,31 @@
+package Program;
+import entities.Employee;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class AppEmployee {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee emp = new Employee();
+
+        System.out.println("Name:");
+        emp.name = sc.nextLine();
+        System.out.println("Gross salary; ");
+        emp.grossSalary = sc.nextDouble();
+        System.out.println("Tax: ");
+        emp.tax = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Employee " + emp);
+        System.out.println();
+        System.out.println("which percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        emp.increaseSalary(percentage);
+
+        System.out.println();
+        System.out.println("Updated data: " + emp);
+        sc.close();
+    }
+}
---
+++ EstudoPooUdemy/src/Program/appStudent.java
@@ -0,0 +1,28 @@
+package Program;
+import entities.Student;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class appStudent {
+    public static void main(String [] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Student student = new Student();
+
+        student.name = sc.nextLine();
+        student.grade1 = sc.nextDouble();
+        student.grade2 = sc.nextDouble();
+        student.grade3 = sc.nextDouble();
+
+        System.out.println("FINAL GRADE: %.2f%n", student.finalGrade());
+
+        if (student.finalGrade() < 60.0) {
+            System.out.println("Failed.");
+            System.out.println("Missing %.2f Point%n", student.missingPoints());
+        }else {
+            System.out.println("Pass.");
+        }
+        sc.close();
+    }
+}
