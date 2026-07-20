29f87c775a186abfcb6f4d8fb4cf000b9c1ee6c0
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-07-20 09:19:13 -0300

    métodos não estáticos

--- EstudoPooUdemy/MembrosEstatiticos/src/App/Main.java
+++ EstudoPooUdemy/MembrosEstatiticos/src/App/Main.java
@@ -1,36 +1,29 @@
+package App;
+
 import java.util.Locale;
 import java.util.Scanner;
+import util.Calculator;
 /*fazendo o método na própria classe*/
 
-public class main {
-
-    public static final double PI = 3.14159;
+public class Main {
 
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        Calculator calc = new Calculator(); // Instanciando o Objeto
 
         System.out.println("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = circumference(radius);
-
-        double v = volume(radius);
+        double c = calc.circumference(radius);
+        double v = calc.volume(radius);// chamando métodos circumference e volume
 
         System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("Pi volume: %.2f%n", PI);
+        System.out.printf("Pi volume: %.2f%n", calc.PI );
 
         sc.close();
 
-    }
-
-    public static double circumference(double radius) {
-        return 2.0 * PI * radius;
-    }
-
-    public static double volume(double radius) {
-        return 4.0 * PI * radius * radius * radius / 3;
     }
 
 }
