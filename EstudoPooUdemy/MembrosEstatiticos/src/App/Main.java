efbdef50644e4389897c05257137c33902e1db12
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-07-20 09:30:04 -0300

    Chmando métodos sem instaciar objetos

--- EstudoPooUdemy/MembrosEstatiticos/src/App/Main.java
+++ EstudoPooUdemy/MembrosEstatiticos/src/App/Main.java
@@ -10,17 +10,16 @@ public class Main {
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Calculator calc = new Calculator(); // Instanciando o Objeto
 
         System.out.println("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = calc.circumference(radius);
-        double v = calc.volume(radius);// chamando métodos circumference e volume
+        double c = Calculator.circumference(radius);
+        double v = Calculator.volume(radius);// chamando métodos circumference e volume
 
         System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("Pi volume: %.2f%n", calc.PI );
+        System.out.printf("Pi volume: %.2f%n", Calculator.PI );
 
         sc.close();
 
