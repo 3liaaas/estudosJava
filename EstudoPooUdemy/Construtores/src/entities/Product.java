bf339e21909e81d2c9a8e83fd52ed1f260a66bf1
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-07-22 13:02:03 -0300

    treinando

---
+++ EstudoPooUdemy/Construtores/src/entities/Product.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public static double TotalValueStock(){
+        double total = 0.0;
+        return total;
+    }
+
+    public static void AddProducts () {
+
+    }
+
+    public static void RemoveProducts(){
+
+    }
+}
